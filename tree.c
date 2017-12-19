7ee1cce15964f0207ef4edec489893d6cd3bfdd7
HelicopterHig <32367339+HelicopterHig@users.noreply.github.com>
2017-12-19 03:30:08 +0300

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -163,7 +163,8 @@ int main(int argc, char *argv[])
             else
                 printf("%s", "False commad\n");
         }
- else if (!strcmp(command, "rem")){
+
+        else if (!strcmp(command, "rem")){
             scanf("%s", command);
             if (!strcmp(command, "whole")){
                 removetree(tree);
@@ -352,6 +353,7 @@ int rmel(node *tree, int numtos, char typedel){
                     if (tree->left->item == numtos){
                         removetree(tree->left);
                         tree->left = 0;
+
                         tmp = flag =1;
                     }
                 }
@@ -364,3 +366,232 @@ int rmel(node *tree, int numtos, char typedel){
                     }
                 }
         }
+        else
+            printf("False 3 parameter of removeelemnt\n");
+    }
+    else
+        return 0;
+    if (!flag) tmp = rmel(tree->left, numtos, typedel);
+    if (!flag) tmp = rmel(tree->right, numtos, typedel);
+    if (!tree->parent) flag =0;
+    return tmp;
+}
+
+void removetree(node *tree){
+    if (!tree) return;
+    if (tree->left)
+        removetree(tree->left);
+    if (tree->right)
+        removetree(tree->right);
+    free(tree);
+}
+
+void loadnode(node *tree, FILE *file){
+    int targetn, item;
+    if (!fread(&targetn, sizeof(int), 1, file)) return;
+    if (!fread(&item, sizeof(int), 1, file)) return;
+    addnode(item, tree, targetn, PARENT, 1);
+    loadnode(tree, file);
+}
+
+void writenode(node *tree, FILE *file){
+    if (tree->parent){
+        fwrite(&tree->parent->item, sizeof(int), 1, file);
+        fwrite(&tree->item, sizeof(int), 1, file);
+    }
+    else {
+        fwrite(&tree->item, sizeof(int), 1, file);
+    }
+    if (tree->left) writenode(tree->left, file);
+    if (tree->right) writenode(tree->right, file);
+}
+
+int fheap(node *tree, int curdepth){
+    extern int maxdepth;
+    static int flag, flage1;
+    int tmp = 0;
+    ++curdepth;
+    if (tree){
+        if ((!flag))
+            if (tree->parent)
+                if ((tree->parent->item)<(tree->item))
+                    flag = tmp = 1;
+        if (!flag)
+            if ((curdepth+1)<(maxdepth)){
+                if (!((tree->left)&&(tree->right)))
+                    tmp = flag = 1;
+            }
+        if (curdepth == maxdepth)
+            if (flage1)
+                tmp = flag = 1;
+        if (!flag)
+            tmp = fheap(tree->left, curdepth);
+
+        if (!flag)
+            tmp = fheap(tree->right, curdepth);
+    }
+
+    else{
+        if (!flag)
+            if (curdepth == maxdepth)
+                flage1 = 1;
+    }
+    if (curdepth == 1){
+        flag = 0;
+        flage1 = 0;
+    }
+    return tmp;
+}
+
+int maximaldepth(node *tree, int n){
+    int a = 0, b = 0;
+    if (tree){
+        ++n;
+        a = maximaldepth(tree->left, n);
+        b = maximaldepth(tree->right, n);
+        if (a>n) n=a;
+        if (b>n) n=b;
+    }
+
+    return n;
+}
+
+void statistics(node *tree){
+    extern int lsumm, interiornum, nodesnum;
+    if (tree){
+        ++nodesnum;
+        if ((tree->right)||(tree->left))
+            ++interiornum;
+        else
+            ++lsumm;
+        if (tree->right)
+            statistics(tree->right);
+        if (tree->left)
+            statistics(tree->left);
+    }
+}
+
+node* createroot(int item){
+    node *temp;
+    temp = (node*)malloc(sizeof(node));
+    temp->left = temp->right = temp->parent = 0;
+    temp->item = item;
+    return temp;
+}
+
+int addnode(int item, node *current, int target, char type, int launchflag){
+    static int flag;
+    int ntmp = 0;
+    node *temp;
+
+    if (flag) return 1;
+    if (!current){
+        printf("node is empty\n");
+        return 0;
+    }
+
+    if (type == PARENT){
+        if (current->item == target){
+            if (current->left == 0){
+                temp = (node*)malloc(sizeof(node));
+                temp->left = temp->right = 0;
+                temp->parent = current;
+                current->left = temp;
+                temp->item = item;
+                flag = ntmp = 1;
+            }
+            else if (current->right == 0){
+                temp = (node*)malloc(sizeof(node));
+                temp->left = temp->right = 0;
+                temp->parent = current;
+                current->right = temp;
+                temp->item = item;
+                flag = ntmp = 1;
+            }
+        }
+
+        if (!flag){
+            if (current->left){
+                if (current->right){
+                    ntmp = addnode(item, current->left, target, PARENT, 0);
+                    if (!flag) ntmp = addnode(item, current->right, target, PARENT, 0);
+                }
+                else {
+                    ntmp = addnode(item, current->left, target, PARENT, 0);
+                }
+            }
+            else {
+                if (current->right)
+                    ntmp = addnode(item, current->right, target, PARENT, 0);
+            }
+        }
+    }
+
+    else if (type == SIBLING){
+        if (current->left){
+            if (current->right){
+                ntmp = addnode(item, current->left, target, SIBLING, 0);
+                if (!flag) ntmp = addnode(item, current->right, target, SIBLING, 0);
+            }
+            else{
+                if (current->left->item == target){
+                    temp = (node*)malloc(sizeof(node));
+                    temp->left = temp->right = 0;
+                    temp->parent = current;
+                    current->right = temp;
+                    temp->item = item;
+                    flag = ntmp = 1;
+                }
+                else{
+                    ntmp = addnode(item, current->left, target, SIBLING, 0);
+                }
+            }
+        }
+        else if (current->right){
+            if (current->left){
+                ntmp = addnode(item, current->left, target, SIBLING, 0);
+                if (!flag) ntmp = addnode(item, current->right, target, SIBLING, 0);
+            }
+            else {
+                if (current->right->item == target){
+                    temp = (node*)malloc(sizeof(node));
+                    temp->left = temp->right = 0;
+                    temp->parent = current;
+                    current->left = temp;
+                    temp->item = item;
+                    flag = ntmp = 1;
+                }
+                else {
+                    ntmp = addnode(item, current->right, target, SIBLING, 0);
+                }
+            }
+        }
+        else
+            return 0;
+    }
+    else {
+        printf("%s", "False 4 argument of addnode\n");
+        return 0;
+    }
+    if ((flag)&&(launchflag)) flag = 0;
+    return (ntmp);
+}
+
+void printmas(node *tree){
+    if (tree){
+        printmas(tree->left);
+        printf("%d", tree->item);
+        printmas(tree->right);
+    }
+}
+
+void checkprint(node *tree, int n){
+    int i;
+    if (tree){
+        checkprint(tree->right, n+3);
+        for (i=0; i<n; i++)
+            putchar(' ');
+        printf("%d\n", tree->item);
+        checkprint(tree->left, n+3);
+    }
+}
